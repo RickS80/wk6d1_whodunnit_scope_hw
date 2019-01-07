e4f49314b3d9c6d50fe0633532d1070335b44bfc
Rick Symington <rick.symington@gmail.com>
2019-01-07 16:33:35 +0000

    Episode 1-3 complete

--- episodehomework.js
+++ episodehomework.js
@@ -1,12 +1,54 @@
-const scenario = {
-  murderer: 'Miss Scarlet',
-  room: 'Library',
-  weapon: 'Rope'
-};
+// Episode 1
+//
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Library',
+//   weapon: 'Rope'
+// };
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${scenario.murderer}.`;
+// }
+//
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// The murder is Miss Scarlet. The const creates a variable that cannot be changed in block scope. However
+// verdict scope can look outward to the outer scoopes of declarerMurderer and scenario
+
+// Episode 2
+
+// const murderer = 'Professor Plum';
+//
+// const changeMurderer = function() {
+//   murderer = 'Mrs. Peacock';
+// }
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// This will produce an error as a constant variable cannot be changed. If murderer was changed
+// to 'let' then it would produce Mrs Peacock
+
+// Episode 3
+
+let murderer = 'Professor Plum';
 
 const declareMurderer = function() {
-  return `The murderer is ${scenario.murderer}.`;
+  let murderer = 'Mrs. Peacock';
+  return `The murderer is ${murderer}.`;
 }
 
-const verdict = declareMurderer();
-console.log(verdict);
+const firstVerdict = declareMurderer();
+console.log('First Verdict: ', firstVerdict);
+
+const secondVerdict = `The murderer is ${murderer}.`;
+console.log('Second Verdict: ', secondVerdict);
+
+// The first verdict will be Mrs Peacock, the second verdict will be Prof Plum. In the first verdict the
+// 'let' scope allows for the murderer to be changed by the declareMurderer function.
